6e01404b472f1d9d0aeb83ca4596aad6b9f4d235
jpascas <jpascas@users.noreply.github.com>
2018-08-19 03:52:38 -0500

    refactor of test names
    refactor of test names

--- src/test/java/com/oocode/AssertionTest.java
+++ src/test/java/com/oocode/AssertionTest.java
@@ -4,14 +4,13 @@ import org.junit.Test;
 
 import java.util.List;
 import static java.util.Arrays.asList;
-import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class AssertionTest {
 
     @Test
-    public void CheckThatTwoListAreEqualsIncludingTheOrderOfTheirElements() {
+    public void CheckThat_TwoListAreEqualsIncludingTheOrderOfTheirElements() {
         List<String> expectedResult = asList("Hello Wally", "Hello World");
         List<String> realResult = asList("Hello Wally", "Hello World");
 
@@ -21,11 +20,11 @@ public class AssertionTest {
     }
 
     @Test
-    public void CheckThatTwoListAreNotEqualsIfOrderOfElementsIsDiferent() {
+    public void CheckThat_TwoListAreNotEquals_IfOrderOfElementsIsDiferent() {
         List<String> expectedResult = asList("Hello Wally", "Hello World");
         List<String> realResult = asList("Hello World" ,"Hello Wally");
 
-        assertNotEquals("Check that both list are on the same order",
+        assertNotEquals("Check that both list are NOT on the same order",
                 expectedResult,
                 realResult);
     }
